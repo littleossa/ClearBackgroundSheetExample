586b0860c261caf38711f9629ec0ae6b00ef37ea
littleossa <littleossa@gmail.com>
2022-09-30 05:41:55 +0900

    Initial Commit

---
+++ ClearBackgroundSheetExample/ClearBackgroundSheetExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClearBackgroundSheetExampleApp.swift
+//  ClearBackgroundSheetExample
+//
+//  Created by 平岡修 on 2022/09/30.
+//
+
+import SwiftUI
+
+@main
+struct ClearBackgroundSheetExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
